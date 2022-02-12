130f091da0ea9fcc3f63c5b7600da4e2cd51eb02
IrocNinoNiel <iroc.niel123@gmail.com>
2022-02-12 16:08:32 +0800

    First practice for vue js using SASS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Practice1/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Practice1 from './components/Practice1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Practice1
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+
 </style>
---
+++ src/components/Practice1.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="">
+        <form action="action_page.php" method="post">
+
+
+            <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+
+                <button type="submit">Login</button>
+                <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+            </div>
+
+            <div class="container" style="background-color:#f1f1f1">
+                <div class="container2">hello</div>
+                <button type="button" class="cancelbtn">Cancel</button>
+                <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Practice1',
+}
+</script>
+
+<style lang="sass">
+$breakpoint: 300px;
+$border-config: 3px solid #f1f1f1
+
+%default-config
+    width: 100%
+    padding: 12px 20px
+    margin: 8px 0
+    display: inline-block
+    border: 1px solid #ccc
+    box-sizing: border-box
+
+form
+    border: $border-config
+
+input[type=text], input[type=password] 
+    @extend %default-config
+
+button 
+    background-color: #04AA6D
+    color: white
+    padding: 14px 20px
+    margin: 8px 0
+    border: none
+    cursor: pointer
+    width: 100%
+
+button:hover 
+    opacity: 0.8
+
+.cancelbtn 
+    width: auto
+    padding: 10px 18px
+    background-color: #f44336
+
+
+.container 
+    padding: 16px
+    .container2
+        color: blue
+        margin: 8px 0
+
+
+span.psw 
+    float: right;
+    padding-top: 16px;
+
+@media (max-width: $breakpoint) 
+    span.psw 
+        display: block
+        float: none
+  
+    .cancelbtn 
+        width: 100%
+
+</style>
